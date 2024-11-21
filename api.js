df6a1fe0a5774712babad269dc749997981f58a1
assakali <assakali.mehdi@gmail.com>
2024-11-21 14:56:29 +0100

    finition du projet site web

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 const API_URL = "http://localhost:8000/api/v1/titles/";
-const IMAGE_PAR_DEFAUT = "./assets/pexels-cottonbro-3945317.jpg"
+const IMAGE_PAR_DEFAUT = "assets/images/bobine_film.jpg"
 
 // Fonction qui recupère les données de l'api.
 async function fetchFilms(url) {
@@ -15,6 +15,8 @@ async function fetchFilms(url) {
     }  
 }
 
+
+
 //Fonction pour charger le meilleur selon sont score imbd
 async function chargeMeilleurFilm() {
     const data = await fetchFilms(`${API_URL}?sort_by=-imdb_score&page_size=1`);      
@@ -24,8 +26,7 @@ async function chargeMeilleurFilm() {
         const details = await fetchFilms(meilleurFilm.url)
 
 
-        document.querySelector(".meilleur-film-titre").textContent = "Meilleur film";
-        document.querySelector(".meilleur-film-titre").textContent = details.title;         
+        document.querySelector(".meilleur-film-categorie").textContent = "Meilleur film";           
         document.querySelector(".meilleur-film-détails .meilleur-film-titre").textContent = details.title;         
         document.querySelector(".meilleur-film-resume").textContent = details.long_description;
         document.querySelector(".meilleur-film-image").src = details.image_url; 
@@ -65,13 +66,9 @@ async function chargerFilmsMieuxNotes() {
             const filmElement = document.createElement("div");
             filmElement.classList.add("fiche-de-film");
             
-            let imageUrl = details.image_url;
-            if(!imageUrl || imageUrl.trim() ==="" ){
-                imageUrl = IMAGE_PAR_DEFAUT;
-            }
-
             filmElement.innerHTML =` 
-                <img src="${imageUrl}" alt="Affcihe de ${details.title}" class="image-film">
+                <img src="${details.image_url}" alt="Affcihe de ${details.title}" class="image-film"
+                 onerror="this.onerror=null; this.src='${IMAGE_PAR_DEFAUT}';">
                 <div class="overlay">
                     <h3 class="titre-film">${details.title}</h3>
                     <button class="film-boutton">Détails</button>
@@ -115,8 +112,7 @@ async function chargerGenres() {
                 selecteur.appendChild(option)
             });
 
-            const premiereCategorie = selecteur.options[0].value;
-            selecteur.value = premiereCategorie;
+            
                    
     } catch (error){
         console.error("Erreur lors du chargement des genres:", error)
@@ -139,31 +135,27 @@ async function chargerFilmsParCategorie(categorie, containerSelector) {
 
         detailsArray.forEach(details =>  {           
             if (details){            
-            const filmElement = document.createElement("div");
-            filmElement.classList.add("fiche-de-film");
-
-            let imageUrl = details.image_url;
-            if(!imageUrl || imageUrl.trim() ==="" ){
-                imageUrl = "./assets/pexels-cottonbro-3945317.jpg";                
-            }
+                const filmElement = document.createElement("div");
+                filmElement.classList.add("fiche-de-film");          
+                
             
-
-            filmElement.innerHTML =` 
-                <img src="${imageUrl}" alt="Affcihe de ${details.title}" class="image-film">
-                <div class="overlay">
-                    <h3 class="titre-film">${details.title}</h3>
-                    <button class="film-boutton">Détails</button>
-                </div>
-            `;
-            filmsContainer.appendChild(filmElement);
-
-            filmElement.querySelector(".film-boutton").addEventListener('click', () => {
-                ouvrirModale(details);
-            });
+                
+                filmElement.innerHTML =` 
+                    <img src="${details.image_url}" alt="Affcihe de ${details.title}" class="image-film"
+                     onerror="this.onerror=null; this.src='${IMAGE_PAR_DEFAUT}';">
+                    <div class="overlay">
+                        <h3 class="titre-film">${details.title}</h3>
+                        <button class="film-boutton">Détails</button>
+                    </div>
+                `;
+                filmsContainer.appendChild(filmElement);
+                console.clear()
+                filmElement.querySelector(".film-boutton").addEventListener('click', () => {
+                    ouvrirModale(details);
+                });
             }
         });
-        console.log(`Nombre total de films après ajout : ${filmsContainer.children.length}`);
-        
+
     }  else {
         console.warn("Aucun film trouvé ou l'API est désactivé")
     }   
@@ -172,25 +164,26 @@ async function chargerFilmsParCategorie(categorie, containerSelector) {
 } 
 
 
-// Charger les films de la catégorie libre à partir du menu déroulant
+
 
 
 // Fonction pour ouvrir la fenêtre modale avec les détails du film
-function ouvrirModale(film) {
-    const maxDescriptionLength = 300;
-    let description = film.long_description || "Description non disponible";
-    if (description.length > maxDescriptionLength){
-        description = description.substring(0, maxDescriptionLength) + "...";
-    }
+function ouvrirModale(film) {    
     document.querySelector(".modale-titre").textContent = film.title;
     document.querySelector(".modale-info").innerHTML= `
     ${film.year || "Année inconnue"} - ${film.genres ? film.genres.join(', ') : "Genre incconus"}<br>
     ${film.rated || "Non classé"} - ${film.duration || "Durée inconnue"} (${film.countries ? film.countries.join(', '): "Pays inconnus"})<br>
     IMDB score : ${film.imdb_score || "NA"}/10
     `;
-    document.querySelector(".modale-réalisateur").innerHTML = `<strong>Réalisaté par:</strong><br> ${film.directors.join(", ")}`;
-    document.querySelector(".modale-image img").src = film.image_url;
-    document.querySelector(".modale-résumé").textContent = description || "Description non disponible";
+    document.querySelector(".modale-réalisateur").innerHTML = `<strong>Réalisaté par:</strong><br> ${film.directors.join(", ")}`;    
+    const imgElement = document.querySelector(".modale-image img");
+    imgElement.src = film.image_url;
+    imgElement.alt = `Affiche de ${film.title}`;
+    imgElement.onerror = () => {
+        imgElement.src = IMAGE_PAR_DEFAUT;
+        
+    };
+    document.querySelector(".modale-résumé").textContent = film.long_description || "Description non disponible";
     document.querySelector(".modale-acteurs").innerHTML = `<strong>Avec:</strong><br> ${film.actors.join(", ")}`;
 
     document.getElementById("modale").style.display = "flex";
